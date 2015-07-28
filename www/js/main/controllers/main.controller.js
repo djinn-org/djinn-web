3f977c92025ce5980bfb0829ed9900739fcdadf8
Laurent Sanchez <laurent.sanchez-ext@amundi.com>
2015-07-28 14:54:26 +0200

    vertical center login

--- www/js/main/controllers/main.controller.js
+++ www/js/main/controllers/main.controller.js
@@ -8,11 +8,19 @@
 
       $scope.main = {
         goState: null,
+        open: false,
+        close: true,
 
         goHome: function() {
           if($scope.main.goState) {
             $state.go($scope.main.goState);
           }
+        },
+
+        logout: function() {
+          $scope.main.close = true;
+          $scope.main.open  = false;
+          $state.go('main.login');
         }
       };
 
